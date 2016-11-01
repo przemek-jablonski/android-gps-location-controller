0c33c85d3fcc3617e35f87f98c8bb0d26f01f4ee
sharaquss <sharaquss@gmail.com>
2016-11-02 00:56:35 +0100

    quick menu, morphing quick actions toolbar

--- app/src/main/java/com/android/szparag/gpslocationcontroller/MainActivity.java
+++ app/src/main/java/com/android/szparag/gpslocationcontroller/MainActivity.java
@@ -1,24 +1,42 @@
 package com.android.szparag.gpslocationcontroller;
 
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.github.rubensousa.floatingtoolbar.FloatingToolbar;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity{
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
+
+    @BindView(R.id.morphingFabToolbar)
+    FloatingToolbar morphingFabToolbar;
+
+    @BindView(R.id.activity_main_appbar_layout)
+    AppBarLayout appBarLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -31,9 +49,8 @@ public class MainActivity extends AppCompatActivity{
             }
         });
 
-//        MapFragment mapFragment = (MapFragment) getFragmentManager()
-//                .findFragmentById(R.id.map);
-//        mapFragment.getMapAsync(this);
+        morphingFabToolbar.attachFab(fab);
+        morphingFabToolbar.attachAppBarLayout(appBarLayout);
 
     }
 }
